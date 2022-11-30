ec75a1861ec510395c64e4192d9a65f149427cd3
Ragul Moorthy <99385305+RagulMoorthy@users.noreply.github.com>
2022-11-30 19:44:43 +0530

    Update Addition.cs

--- C#/Addition.cs
+++ C#/Addition.cs
@@ -1,1 +1,15 @@
-
+using System;
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 15;
+            int b = 10;
+            int c = 20;
+            int d = a + b + c;
+            Console.Write(d);
+        }
+    }
+}
